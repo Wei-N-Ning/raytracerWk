9c2517e6e933c7b580e81726058dade7d1313bc4
Wei Ning (DA) <wei.ning@digitalasset.com>
2021-02-04 11:58:42 +1100

    exam1-take2: some look dev

--- src/exam1/test_render_take2.cpp
+++ src/exam1/test_render_take2.cpp
@@ -591,27 +591,28 @@ OptError ensure_reflection_multiple_sphere()
     }
 }
 
+// NOTE: I've done some lookdev here
 OptError ensure_refraction_and_glass_surface()
 {
     constexpr size_t x = 800;
     constexpr size_t y = 400;
     constexpr size_t ss = 64;
     ImageDriver id{ x, y, ss };  // 3 : 2
-    Lambertian diffuseBlue{ { 0.57, 0.53, 0.54 } };
+    Lambertian diffuseBlue{ { 0.6, 0.66, 0.66 } };
     Metal metalBase{ { 0.82, 0.61, 0.53 }, 0.8 };
-    Metal metalBronze{ { 0.68, 0.7, 0.4 }, 0.4 };
-    Metal metalIron{ { 0.6, 0.6, 0.67 }, 0.13 };
+    Metal metalBronze{ { 0.68, 0.7, 0.4 }, 0.35 };
+    Metal metalIron{ { 0.6, 0.6, 0.67 }, 0.06 };
     Dielectrics dielectrics{};
 
-    Sphere s1{ Vec3{ 0, 0, -3.4 }, 0.5, &diffuseBlue };
-    Sphere s2{ Vec3{ 1.2, 0, -3.95 }, 0.5, &metalBronze };
-    Sphere s3{ Vec3{ -1.2, 0, -3.7 }, 0.5, &metalIron };
+    Sphere s1{ Vec3{ 0, -0.004, -3.4 }, 0.5, &diffuseBlue };
+    Sphere s2{ Vec3{ 1.2, -0.015, -3.95 }, 0.5, &metalBronze };
+    Sphere s3{ Vec3{ -1.2, -0.015, -3.7 }, 0.5, &metalIron };
 
     Sphere base{ Vec3{ 0, -100.5, -3.6 }, 100, &metalBase };
 
     DualTone bg{ //
                  Color{ 0.09, 0.14, 0.45 },
-                 Color{ 0.9, 0.19, 0.09 },
+                 Color{ 0.9, 0.5, 0.09 },
                  4 };
     Renderer renderer{ bg };
     renderer.add( &s1 );
